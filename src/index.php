bde49d4f5ca90d497ec6463b27772ff89819034f
rguezgiova <grodriguez.inf@gmail.com>
2022-01-24 19:42:52 +0000

    Subido ficheros

--- src/index.php
+++ src/index.php
@@ -1,5 +1,5 @@
 <?php
     echo "<h1>Página Principal cataplas</h1>";
     echo "<p>Giovanni</p>";
-    echo "<img src='https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg' alt="Image">";
+    echo "<img src="https://repository-images.githubusercontent.com/21969270/25b81280-c039-11e9-9a7c-2c2222c231b5" alt="Image">";
 ?>
