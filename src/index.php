3b7670304225bfc3e8e6fc4d5d126c5d7ed40518
rguezgiova <grodriguez.inf@gmail.com>
2022-01-24 16:59:11 +0000

    Subido ficheros

--- src/index.php
+++ src/index.php
@@ -1,5 +1,5 @@
 <?php
     echo "<h1>Página Principal cataplas</h1>";
     echo "<p>Giovanni</p>";
-    echo "<img src="https://1.bp.blogspot.com/-79DdxzZkDog/T76QV6v5IuI/AAAAAAAAAEY/6DzpGZzsmfA/s320/homerocatolico_456_336.jpg" />"
+    echo "<img src="https://1.bp.blogspot.com/-79DdxzZkDog/T76QV6v5IuI/AAAAAAAAAEY/6DzpGZzsmfA/s320/homerocatolico_456_336.jpg" />";
 ?>
